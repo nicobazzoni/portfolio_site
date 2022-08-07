c1277386b5d39f737340c94f11bc3c0eb154a17a
nico bazzoni <nicobazzoni@gmail.com>
2022-08-06 20:48:28 -0400

    skills section added and motion wrapper, hover over years has bug that needs fix

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -29,9 +29,22 @@ const Work = () => {
   }, [])
 
   const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
 
 
-  }
+  
   return (
     <>
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
@@ -104,4 +117,8 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work');
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  'app__primarybg',
+);
