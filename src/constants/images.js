47ddf920865bc5b5fbcf71437c53ddd0c1d5859b
nico bazzoni <nicobazzoni@gmail.com>
2022-08-07 21:51:06 -0400

    new designs, icon, fixed navbar for testimonial

--- src/constants/images.js
+++ src/constants/images.js
@@ -23,7 +23,7 @@ import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
-import profile from '../assets/headhsot.jpg';
+import profile from '../assets/head.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
@@ -34,6 +34,10 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import tailwind from '../assets/tailwindlogo.png';
+
+import nico from '../assets/nico.png'
+import next from '../assets/nextreactnode.jpeg'
 
 export default {
   email,
@@ -69,4 +73,7 @@ export default {
   nb,
   skype,
   spotify,
+  tailwind,
+  nico,
+
 };
